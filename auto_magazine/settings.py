e473591ad15bd5fc737418d607e838e53b22631a
Jonas Silva <jonaslsa@uio.no>
2022-08-17 22:31:30 +0200

    Static files

--- auto_magazine/settings.py
+++ auto_magazine/settings.py
@@ -123,6 +123,7 @@ APPEND_SLASH = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = env('STATIC_ROOT', default=BASE_DIR / 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
